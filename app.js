c756ae2e42b4089db596180bb937d6250a044942
Soumya <agr.soumya@gmail.com>
2017-03-05 09:39:48 +0530

    event add  not working

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var http = require('https');
+var https = require('https');
+https.post = require('https-post');
 
 var api_url = "https://www.eventbriteapi.com/v3/";
 var authkey = "?token=GXNZMZWKOWG5XSE4FZ2T"
@@ -43,7 +44,8 @@ bot.dialog('/', [
     function(session, results, next) {
     	var questions = {
     "List all events": {},
-    "Show events by date": {}
+    "Show events by date": {},
+    "Add event": {}
 };
 
     	builder.Prompts.choice(session ,"How can i help you today? ", questions, {listStyle: builder.ListStyle.button});
@@ -57,6 +59,10 @@ bot.dialog('/', [
     	else if (results.response.entity== "Show events by date") 
     	{
     		session.beginDialog('/fetchByDate', session.userData.profile);
+    	}
+    	else if(results.response.entity=="Add event")
+    	{
+    		session.beginDialog('/addEvent', session.dialogData.eventDetails);
     	}
     }
 ]);
@@ -77,53 +83,162 @@ bot.dialog('/ensureProfile', [
 
     }
 ]);
-
-bot.dialog('/fetchAllEvents', [
+bot.dialog('/addEvent',[
 	function (session, args, next) {
+		session.dialogData.eventDetails = {};
+		builder.Prompts.text(session, "Event Name");
+
+	},
+	function (session, results, next) {
+		if(results.response){
+			session.dialogData.eventDetails.name = results.response;
+		}
+		next();
+	},
+	function (session, args, next) {
+		builder.Prompts.time(session, "Event start date and time");
+	},
+	function (session, results, next) {
+		session.dialogData.eventDetails.starttime = ((builder.EntityRecognizer.resolveTime([results.response])).toISOString().slice(0,19)+'Z');
+		session.send(session.dialogData.eventDetails.starttime);
+		next();
+	},
+	function (session, args, next) {
+		builder.Prompts.time(session, "Event end date and time");
+	},
+
+	function (session, results, next) {
+		session.dialogData.eventDetails.endtime = ((builder.EntityRecognizer.resolveTime([results.response])).toISOString().slice(0,19)+'Z');
+		session.send(session.dialogData.eventDetails.endtime);
+		next();
+	},
+	function (session, args, next) {
+		// http.post(api_url +"events/"+ authkey + '&event.name.html=' + session.dialogData.eventDetails.name+ '&event.start.utc' + session.dialogData.eventDetails.starttime + '&event.end.utc' + session.dialogData.eventDetails.endtime +'&event.start.timezone'+  '+2232+08822'+ '&event.end.timezone' + '+2232+08822' ,(res) => {
+		//   const statusCode = res.statusCode;
+		//   const contentType = res.headers['content-type'];
+
+		//   var error;
+		//   if (statusCode !== 200) {
+		//     error = new Error(`Request Failed.\n` +
+		//                       `Status Code: ${statusCode}`);
+
+		//   } else if (!/^application\/json/.test(contentType)) {
+		//     error = new Error(`Invalid content-type.\n` +
+		//                       `Expected application/json but received ${contentType}`);
+		//   }
+		//   if (error) {
+		//     session.send(error.message);
+		//     // consume response data to free up memory
+		//     res.resume();
+		    
+		//   }
+		//   res.setEncoding('utf8');
+		//   var  rawData = '';
+		//   res.on('data', (chunk) => rawData += chunk);
+
+		//   res.on('end', () => {
+		//     try {
+		//       var  parsedData = JSON.parse(rawData);
+		//       // console.log(parsedData.events);
+		//       session.send(parsedData);
+
+		      
+		//       console.log(eventlist);
+
+		//     } catch (e) {
+		//       console.log(e.message);
+		//     }
+		//   });
+		// 	}).on('error', (e) => {
+		//   console.log(`Got error: ${e.message}`);
+		// 	});
+		// }
+		console.log('posting!!');
+		var postdata = { 'event.name.html':session.dialogData.eventDetails.name,'event.start.utc':session.dialogData.eventDetails.starttime,'event.start.timezone':'+2232+08822' , 'event.end.utc':session.dialogData.eventDetails.endtime,'event.end.timezone':'+2232+08822','event.currency':'INR'};
+
+		console.log(postdata);
+		https.post(api_url +"events/"+ authkey, { 'event.name.html':session.dialogData.eventDetails.name,'event.start.utc':session.dialogData.eventDetails.starttime,'event.start.timezone':'+2232+08822' , 'event.end.utc':session.dialogData.eventDetails.endtime,'event.end.timezone':'+2232+08822','event.currency':'INR'}, function(res){
+			console.log('posting!!');
+			const statusCode = res.statusCode;
+		  const contentType = res.headers['content-type'];
+
+		  var error;
+		  if (statusCode !== 200) {
+		    error = new Error(`Request Failed.\n` +
+		                      `Status Code: ${statusCode}`);
+		  } else if (!/^application\/json/.test(contentType)) {
+		    error = new Error(`Invalid content-type.\n` +
+		                      `Expected application/json but received ${contentType}`);
+		  }
+		  if (error) {
+		    session.send(error.message);
+		    // consume response data to free up memory
+		    res.resume();
+		    
+		  }
+			res.setEncoding('utf8');
+			var  rawData = '';
+		  	res.on('data'	, (chunk) => rawData += chunk);
+		  	res.on('end', () => {
+			    try {
+			      var  parsedData = JSON.parse(rawData);
+			      console.log(parsedData);
+			      } catch (e) {
+			      console.log(e.message);
+			    }
+		  	});
+		}).on('error', (e) => {
+		  console.log(`Got error: ${e.message}`);
+			});
 		
-  	http.get(api_url +"events/search/"+ authkey, (res) => {
-  const statusCode = res.statusCode;
-  const contentType = res.headers['content-type'];
-
-  var error;
-  if (statusCode !== 200) {
-    error = new Error(`Request Failed.\n` +
-                      `Status Code: ${statusCode}`);
-  } else if (!/^application\/json/.test(contentType)) {
-    error = new Error(`Invalid content-type.\n` +
-                      `Expected application/json but received ${contentType}`);
-  }
-  if (error) {
-    session.send(error.message);
-    // consume response data to free up memory
-    res.resume();
-    
-  }
-  var eventlist = [];
-  res.setEncoding('utf8');
-  var  rawData = '';
-  res.on('data', (chunk) => rawData += chunk);
-
-  res.on('end', () => {
-    try {
-      var  parsedData = JSON.parse(rawData);
-      // console.log(parsedData.events);
-      for (var i = parsedData.events.length - 1; i >= 0; i--) {
-      	eventlist.push(	parsedData.events[i].name.text);
-        session.send(eventlist);
-
-      }
-      console.log(eventlist);
-
-    } catch (e) {
-      console.log(e.message);
-    }
-  });
-	}).on('error', (e) => {
-  console.log(`Got error: ${e.message}`);
-	});
 	}
 ])
+bot.dialog('/fetchAllEvents', [
+	function (session, args, next) {		
+		  https.get(api_url +"events/search/"+ authkey, (res) => {
+		  const statusCode = res.statusCode;
+		  const contentType = res.headers['content-type'];
+
+		  var error;
+		  if (statusCode !== 200) {
+		    error = new Error(`Request Failed.\n` +
+		                      `Status Code: ${statusCode}`);
+		  } else if (!/^application\/json/.test(contentType)) {
+		    error = new Error(`Invalid content-type.\n` +
+		                      `Expected application/json but received ${contentType}`);
+		  }
+		  if (error) {
+		    session.send(error.message);
+		    // consume response data to free up memory
+		    res.resume();
+		    
+		  }
+		  var eventlist = [];
+		  res.setEncoding('utf8');
+		  var  rawData = '';
+		  res.on('data', (chunk) => rawData += chunk);
+
+		  res.on('end', () => {
+		    try {
+		      var  parsedData = JSON.parse(rawData);
+		      // console.log(parsedData.events);
+		      for (var i = parsedData.events.length - 1; i >= 0; i--) {
+		      	eventlist.push(	parsedData.events[i].name.text);
+		        session.send(eventlist);
+
+		      }
+		      console.log(eventlist);
+
+		    } catch (e) {
+		      console.log(e.message);
+		    }
+		  });
+			}).on('error', (e) => {
+		  console.log(`Got error: ${e.message}`);
+			});
+	}
+])
+
 bot.dialog('/fetchByDate', [
 	function (session, args, next) {
 	builder.Prompts.time(session, "Enter the date ");
